4efaf60bbb81ba8d2cc8f487e075042d1f4923f2
prathusingh <prathusingh21@gmail.com>
2023-09-05 15:23:11 -0400

    feat: added styles for image details

--- src/search/ImageDetails.tsx
+++ src/search/ImageDetails.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+import ImageCard from '../elements/ImageCard';
+
+import './ImageDetails.scss';
 
 const ImageDetails: React.FC = () => {
+  const location = useLocation();
+  const user = location.state?.user;
+  const tags = location.state?.tags;
+
   return (
-    <div>Image Details</div>
+    <section className="search-details-wrapper">
+      <div className="user-details">
+        <p>User name is: {user.name} and id is: {user.id}</p>
+        <ImageCard 
+            url={user.image}
+            height={100} 
+            width={100}
+        />
+      </div>
+      <ul className="image-tags">
+        Image tags are:
+        {
+          tags.map((tag: string) => {
+            return (
+              <li>
+                {tag}
+              </li>
+            )
+          })
+        }
+      </ul>
+    </section>
   )
 }
 
